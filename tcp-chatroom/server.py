9ad9018923ee92e935f29b04ab96e00714820523
Rory Kane <roarigami@gmail.com>
2024-06-19 08:52:21 -0400

    Main receive function
    Receive function lets the server accept connections and returns the client and the address of the client. Currently accepts all connections.

--- tcp-chatroom/server.py
+++ tcp-chatroom/server.py
@@ -29,3 +29,19 @@ def handle(client):
             broadcast(f'{nickname} left the chat!'.encode('ascii'))
             nicknames.remove(nickanme)
             break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}')
+
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        clients.append(client)
+
+        print(f'Nickname of the client is {nickname}')
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        client.send('Connected to the server')
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
