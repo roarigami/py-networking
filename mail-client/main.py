96d0ae730740979afaf3c203b417b9dd010e3409
Rory Kane <roarigami@gmail.com>
2024-06-19 14:05:00 -0400

    SMTP protocol
    Using smtp protocol to send mail from one account to another. Must have auth credentials of sender account.

---
+++ mail-client/main.py
@@ -0,0 +1,10 @@
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 25)
+
+server.ehlo()
+
+with open('password.txt', 'r') as f:
+    password = f.read()
+
+server.login('example@gmail.com', password)
