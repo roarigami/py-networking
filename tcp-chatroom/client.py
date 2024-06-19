fa776587e46a52067cf1547cbeba23c03bfc2d9a
Rory Kane <roarigami@gmail.com>
2024-06-19 10:47:36 -0400

    Client receive
    Receive function is constantly running to receive the data from the server.

--- tcp-chatroom/client.py
+++ tcp-chatroom/client.py
@@ -3,3 +3,16 @@ import threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message = 'NICK'
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occured")
+            client.close()
+            break
