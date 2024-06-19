e6d189e4d15058c302a4cbac014812574fab07fc
Rory Kane <roarigami@gmail.com>
2024-06-19 14:32:50 -0400

    Message
    Used MIME to add header and body text to the message object.

--- mail-client/main.py
+++ mail-client/main.py
@@ -1,4 +1,8 @@
 import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 
 server = smtplib.SMTP('smtp.gmail.com', 25)
 
@@ -8,3 +12,13 @@ with open('password.txt', 'r') as f:
     password = f.read()
 
 server.login('example@gmail.com', password)
+
+msg = MIMEMultipart()
+msg['From'] = 'User'
+msg['To'] = 'text@gmail.com'
+msg['Subject'] = 'Submit your packets at once.. Just kidding this is a test'
+
+with open('message.txt', 'r') as f:
+    message = f.read()
+
+msg.attach(MIMEText(message, 'plain'))
