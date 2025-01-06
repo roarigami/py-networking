05595005b82e1a00464638bf7785ba07cdfd05f8
Rory Kane <roarigami@gmail.com>
2025-01-06 12:36:30 -0500

    Calling parent constructor

--- browser/main.py
+++ browser/main.py
@@ -1,4 +1,10 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from PyQt5.QtWebEngineWidgets import * 
+from PyQt5.QtWebEngineWidgets import *
+
+
+class MyWebBrowser(QMainWindow):
+
+    def __init__(self):
+        super(MyWebBrowser, self).__init__(*args, *kwargs)
